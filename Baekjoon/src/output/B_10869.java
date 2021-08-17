e389c6653775c68732f5fe71836590596a5d59c9
Gwonhyesoo <kysn22@naver.com>
2021-08-17 23:16:53 +0900

    10869

---
+++ Baekjoon/src/output/B_10869.java
@@ -0,0 +1,25 @@
+package output;
+
+import java.util.Scanner;
+
+public class B_10869 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		int a, b ;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		
+
+	}
+
+}
