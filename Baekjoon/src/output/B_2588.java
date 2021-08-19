bbe0dc58bd5b6c881db7e17011eb58c06cea602b
Gwonhyesoo <kysn22@naver.com>
2021-08-19 21:05:17 +0900

    2588

---
+++ Baekjoon/src/output/B_2588.java
@@ -0,0 +1,21 @@
+package output;
+
+import java.util.Scanner;
+
+public class B_2588 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+				
+		int a, b;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a * ((b % 100) % 10));
+		System.out.println(a * ((b % 100) / 10));
+		System.out.println(a * (b / 100));
+		System.out.println(a * b);
+	}
+
+}
