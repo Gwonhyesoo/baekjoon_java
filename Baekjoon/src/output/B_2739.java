c8c34d2a52ee2789a2498b9ffa803dd7b227f034
Gwonhyesoo <kysn22@naver.com>
2021-08-20 21:15:27 +0900

    2739 : 구구단

---
+++ Baekjoon/src/output/B_2739.java
@@ -0,0 +1,19 @@
+package output;
+
+import java.util.Scanner;
+
+public class B_2739 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		int a;
+		a= sc.nextInt();
+		
+		for (int i=1; i <10; i++ ) {
+			System.out.println(a+" * "+i+" = "+a*i);
+		}
+
+	}
+
+}
